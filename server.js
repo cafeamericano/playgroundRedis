63abd99891e7b0b046b619623d9136daf26c50f8
Matthew <mfarmer5102@gmail.com>
2019-09-27 19:46:18 -0400

    Implemented ability to add hash and set multiple key-values with a single route

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//Require ////////////////////////////////////////////////////////////
+
 //Express
 var express = require("express");
 var app = express();
@@ -6,16 +8,52 @@ var app = express();
 var redis = require("redis");
 var client = redis.createClient();
 
-//Routes
+//Routes ////////////////////////////////////////////////////////////
 app.get("/", (req, res) => {
   res.send("yay!");
 });
 
+//Pull first name
 app.get("/firstname", (req, res) => {
   client.get("firstName", function(err, reply) {
-    res.send(reply)
+    res.send(reply);
   });
 });
 
-//Start server
-app.listen(5050, () => console.log(`Listening on Port 5050.`));
+//Pull value from string
+app.get("/:search_key", (req, res) => {
+  client.get(req.params.search_key, function(err, reply) {
+    res.send(reply);
+  });
+});
+
+//Pull keys and values from hash
+app.get("/username/:search_key", (req, res) => {
+  client.hgetall("username:" + req.params.search_key, function(err, reply) {
+    res.json(reply);
+  });
+});
+
+//Pull keys and values from hash
+app.get(
+  "/newhash/:username/:firstname/:lastname/:favoritecolor",
+  (req, res) => {
+    console.log("route hit for adding new hash");
+    let query = [
+      `username:${req.params.username}`,
+      `firstname`,
+      `${req.params.firstname}`,
+      `lastname`,
+      `${req.params.lastname}`,
+      `favoritecolor`,
+      `${req.params.favoritecolor}`
+    ];
+    console.log(query);
+    client.hmset(query, function(err, reply) {
+      res.send(reply);
+    });
+  }
+);
+
+//Start Server ////////////////////////////////////////////////////////////
+app.listen(5051, () => console.log(`Listening on Port 5051.`));
